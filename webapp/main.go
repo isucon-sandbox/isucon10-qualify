5a7a98478164563ceac17c827e2a69f2f612b892
usagrada <usagrada.kind@gmail.com>
2023-06-24 16:59:17 +0900

    logを確認したい

--- webapp/main.go
+++ webapp/main.go
@@ -889,11 +889,13 @@ func searchEstateNazotte(c echo.Context) error {
 	}
 
 	b := coordinates.getBoundingBox()
-	// estatesInBoundingBox := []Estate{}
+	estatesInBoundingBox := []Estate{}
 
-	estatesInPolygon := []Estate{}
-	query := `SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? AND ST_Contains(ST_PolygonFromText(?), ST_GeomFromText(POINT(estate.latitude, estate.longitude))) ORDER BY popularity DESC, id ASC LIMIT ?`
-	err = db.Select(&estatesInPolygon, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude, coordinates.coordinatesToText(), NazotteLimit)
+	query := `SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? ORDER BY popularity DESC, id ASC`
+	err = db.Select(&estatesInBoundingBox, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude)
+	// estatesInPolygon := []Estate{}
+	// query := `SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? AND ST_Contains(ST_PolygonFromText(?), ST_GeomFromText(POINT(estate.latitude, estate.longitude))) ORDER BY popularity DESC, id ASC LIMIT ?`
+	// err = db.Select(&estatesInPolygon, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude, coordinates.coordinatesToText(), NazotteLimit)
 	if err == sql.ErrNoRows {
 		c.Echo().Logger.Infof("select * from estate where latitude ...", err)
 		return c.JSON(http.StatusOK, EstateSearchResponse{Count: 0, Estates: []Estate{}})
@@ -902,33 +904,35 @@ func searchEstateNazotte(c echo.Context) error {
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	// estatesInPolygon := []Estate{}
-	// for _, estate := range estatesInBoundingBox {
-	// 	validatedEstate := Estate{}
+	fmt.Println()
 
-	// 	point := fmt.Sprintf("'POINT(%f %f)'", estate.Latitude, estate.Longitude)
-	// 	query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`, coordinates.coordinatesToText(), point)
-	// 	err = db.Get(&validatedEstate, query, estate.ID)
-	// 	if err != nil {
-	// 		if err == sql.ErrNoRows {
-	// 			continue
-	// 		} else {
-	// 			c.Echo().Logger.Errorf("db access is failed on executing validate if estate is in polygon : %v", err)
-	// 			return c.NoContent(http.StatusInternalServerError)
-	// 		}
-	// 	} else {
-	// 		estatesInPolygon = append(estatesInPolygon, validatedEstate)
-	// 	}
-	// }
+	estatesInPolygon := []Estate{}
+	for _, estate := range estatesInBoundingBox {
+		validatedEstate := Estate{}
+
+		point := fmt.Sprintf("'POINT(%f %f)'", estate.Latitude, estate.Longitude)
+		query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`, coordinates.coordinatesToText(), point)
+		err = db.Get(&validatedEstate, query, estate.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				continue
+			} else {
+				c.Echo().Logger.Errorf("db access is failed on executing validate if estate is in polygon : %v", err)
+				return c.NoContent(http.StatusInternalServerError)
+			}
+		} else {
+			estatesInPolygon = append(estatesInPolygon, validatedEstate)
+		}
+	}
 
 	var re EstateSearchResponse
 	re.Estates = []Estate{}
-	// if len(estatesInPolygon) > NazotteLimit {
-	// 	re.Estates = estatesInPolygon[:NazotteLimit]
-	// } else {
+	if len(estatesInPolygon) > NazotteLimit {
+		re.Estates = estatesInPolygon[:NazotteLimit]
+	} else {
+		re.Estates = estatesInPolygon
+	}
 	// re.Estates = estatesInPolygon
-	// }
-	re.Estates = estatesInPolygon
 	re.Count = int64(len(re.Estates))
 
 	return c.JSON(http.StatusOK, re)
